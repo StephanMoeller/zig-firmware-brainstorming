2e41e524a8b28b7c07c2529b78066fa6619f8ff3
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-04-23 10:10:31 +0200

    tests of serialization done

--- zigmkay/src/split_protocol.zig
+++ zigmkay/src/split_protocol.zig
@@ -29,7 +29,36 @@ pub const UartClient = struct {
 
 pub fn readData(_: rp2xxx.uart.UART, _: *[2]u7) bool {} // <= error handling and message discarding will happen here
 pub fn sendData(_: rp2xxx.uart.UART, _: [2]u7) !void {}
-pub fn serialize(_: ProtocolMessage, _: *[2]u7) void {}
-pub fn deserialize(_: *[2]u7) ProtocolMessage {
-    return .{ .KeyPressed = 14 };
+pub fn serialize(msg: ProtocolMessage, buffer: *[2]u7) void {
+    var message_type: u7 = 0;
+    var payload: u7 = 0;
+    switch (msg) {
+        .KeyPressed => |key_index_released| {
+            message_type = 1;
+            payload = key_index_released;
+        },
+        .KeyReleased => |key_index_pressed| {
+            message_type = 2;
+            payload = key_index_pressed;
+        },
+
+        .EncoderValueChanged => |encoder_value| {
+            message_type = 3;
+            payload = encoder_value;
+        },
+    }
+
+    buffer[0] = message_type;
+    buffer[1] = payload;
+}
+
+pub fn deserialize(buffer: *[2]u7) !ProtocolMessage {
+    const message_type = buffer[0];
+    const payload = buffer[1];
+    switch (message_type) {
+        1 => return .{ .KeyPressed = payload },
+        2 => return .{ .KeyReleased = payload },
+        //3 => return .{ .EncoderValueChanged = payload },
+        else => return ProtocolMessage{ .KeyPressed = 0 },
+    }
 }
--- zigmkay/tests/test_split_protocol.zig
+++ zigmkay/tests/test_split_protocol.zig
@@ -7,7 +7,29 @@ test "Serialize and deserialize: KeyPress" {
     const msg = p.ProtocolMessage{ .KeyPressed = 102 };
     var data: [2]u7 = [2]u7{ 0, 0 };
     p.serialize(msg, &data);
-    const msg_again = p.deserialize(&data);
+    const other_msg = try p.deserialize(&data);
 
-    try std.testing.expectEqual(p.ProtocolMessage{ .KeyPressed = 102 }, msg_again);
+    try std.testing.expectEqual(p.ProtocolMessage{ .KeyPressed = 102 }, other_msg);
+}
+
+test "Serialize and deserialize: KeyReleased" {
+    const msg = p.ProtocolMessage{ .KeyReleased = 47 };
+    var data: [2]u7 = [2]u7{ 0, 0 };
+    p.serialize(msg, &data);
+
+    // sanity checking
+    try std.testing.expectEqual(2, data[0]);
+
+    const other_msg = try p.deserialize(&data);
+
+    try std.testing.expectEqual(p.ProtocolMessage{ .KeyReleased = 47 }, other_msg);
+}
+
+test "Serialize and deserialize: EncoderValueChanged" {
+    const msg = p.ProtocolMessage{ .EncoderValueChanged = 3 };
+    var data: [2]u7 = [2]u7{ 0, 0 };
+    p.serialize(msg, &data);
+    const other_msg = try p.deserialize(&data);
+
+    try std.testing.expectEqual(p.ProtocolMessage{ .EncoderValueChanged = 3 }, other_msg);
 }
