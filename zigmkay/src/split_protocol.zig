1c0cabbdf45fa7f226be96c90a0f36af3c239698
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-04-23 11:00:43 +0200

    added mock with testing of the mock

--- zigmkay/src/split_protocol.zig
+++ zigmkay/src/split_protocol.zig
@@ -27,8 +27,46 @@ pub const UartClient = struct {
     }
 };
 
+pub const UartMock = struct {
+    pointer: usize = 0,
+    data: []const u8,
+    pub fn read_next_mocked_value(self: *UartMock) ?u8 {
+        if (self.pointer < self.data.len) {
+            self.pointer += 1;
+            return self.data[self.pointer - 1];
+        } else {
+            return null;
+        }
+    }
+};
+pub const UartWrapper = struct {
+    uart: ?rp2xxx.uart.UART,
+    mock: ?UartMock,
+    pub fn create(uart: rp2xxx.uart.UART) UartWrapper {
+        return UartWrapper{ .uart = uart, .mock = null };
+    }
+    pub fn create_mock(mock: UartMock) UartWrapper {
+        return UartWrapper{ .uart = null, .mock = mock };
+    }
+    const UartUtils = struct {
+        pub fn read_word(self: *const UartWrapper) ?u8 {
+            if (self.uart) |uart| {
+                const byte_or_null: ?u8 = uart.read_word() catch {
+                    uart.clear_errors();
+                    return null;
+                };
+
+                return byte_or_null;
+            } else if (self.mock) |mock| {
+                return mock.read_next_mocked_value();
+            }
+        }
+    };
+};
+
 pub fn readData(_: rp2xxx.uart.UART, _: *[2]u7) bool {} // <= error handling and message discarding will happen here
 pub fn sendData(_: rp2xxx.uart.UART, _: [2]u7) !void {}
+
 pub fn serialize(msg: ProtocolMessage, buffer: *[2]u7) void {
     var message_type: u7 = 0;
     var payload: u7 = 0;
