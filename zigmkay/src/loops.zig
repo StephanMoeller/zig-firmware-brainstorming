f31994939d1bc097f9d601e47f15a461433aaa1a
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-04-23 16:40:38 +0200

    flashed clacky chan

--- zigmkay/src/loops.zig
+++ zigmkay/src/loops.zig
@@ -153,6 +153,7 @@ pub fn run_primary_internal(
 
     // uart byte queue
     var uart_byte_queue = split_protocol.ByteQueue.Create();
+    var uart_helper = split_protocol.UartHelper{};
 
     // USB events
     const usb_command_executor = usb.CreateAndInitUsbCommandExecutor();
@@ -164,7 +165,7 @@ pub fn run_primary_internal(
         // Receive remote changes
         if (uart_or_null) |uart| {
             try receive_from_uart_to_queue(&uart, &uart_byte_queue);
-            if (split_protocol.receiveMessage(&uart_byte_queue)) |msg| {
+            if (uart_helper.receiveMessage(&uart_byte_queue)) |msg| {
                 switch (msg) {
                     .KeyPressed => |key_index| {
                         try matrix_change_queue.enqueue(.{ .key_index = key_index, .pressed = true, .time = current_time });
@@ -215,15 +216,16 @@ pub fn run_secondary_internal(
     const matrix_scanner = comptime matrix_scanning.CreateMatrixScannerType(dimensions, pin_cols, pin_rows, pin_mappings, scanner_settings){};
 
     var uart_byte_queue = split_protocol.ByteQueue.Create();
+    var uart_helper = split_protocol.UartHelper{};
     while (true) {
         const current_time = core.TimeSinceBoot{ .time_since_boot_us = time.get_time_since_boot().to_us() };
         try matrix_scanner.DetectKeyboardChanges(&matrix_change_queue, current_time);
         while (matrix_change_queue.Count() > 0) {
             const matrix_change = try matrix_change_queue.dequeue();
             if (matrix_change.pressed) {
-                try split_protocol.sendMessage(&uart_byte_queue, .{ .KeyPressed = matrix_change.key_index });
+                try uart_helper.sendMessage(&uart_byte_queue, .{ .KeyPressed = matrix_change.key_index });
             } else {
-                try split_protocol.sendMessage(&uart_byte_queue, .{ .KeyReleased = matrix_change.key_index });
+                try uart_helper.sendMessage(&uart_byte_queue, .{ .KeyReleased = matrix_change.key_index });
             }
 
             try send_from_queue_to_uart(&uart, &uart_byte_queue);
