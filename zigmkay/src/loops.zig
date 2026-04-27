ad766178b764b1f56999744cda494fd18a3e22f1
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-04-27 13:42:05 +0200

    moved around stuff in primary config

--- zigmkay/src/loops.zig
+++ zigmkay/src/loops.zig
@@ -42,59 +42,57 @@ pub fn GetPrimarySideConfigType(comptime dimensions: *const core.KeymapDimension
         pub const Runner = struct {
             config: Self,
             pub fn run_unibody(comptime self: Runner) !void {
-                try self.run_primary_internal(null);
+                try run_primary_internal(self.config, null);
             }
 
             pub fn run_primary(comptime self: Runner, uart: rp2xxx.uart.UART) !void {
-                try self.run_primary_internal(uart);
-            }
-
-            fn run_primary_internal(
-                comptime self: Runner,
-                uart_or_null: ?rp2xxx.uart.UART,
-            ) !void {
-                const config = self.config;
-
-                // Data queues
-                var matrix_change_queue = core.MatrixStateChangeQueue.Create();
-                var encoder_change_queue = core.EncoderEventQueue.Create();
-                var usb_command_queue = core.OutputCommandQueue.Create();
-
-                // Input scanning
-                const matrix_scanner = comptime matrix_scanning.CreateMatrixScannerType(dimensions, config.pin_cols, config.pin_rows, config.pin_mappings, config.scanner_settings){};
-                var encoder_scanner = comptime encoder_scanning.CreateEncoderScannerType(config.encoder_configs){};
-
-                // Processing
-                var processor = processing.CreateProcessorType(dimensions, config.keymap, config.side_definition, config.combos, config.custom_functions){
-                    .input_matrix_changes = &matrix_change_queue,
-                    .output_usb_commands = &usb_command_queue,
-                    .encoder_event_changes = &encoder_change_queue,
-                };
-
-                // uart byte queue
-                var uart_receiver = split_protocol.UartReceiveHelper{};
-
-                // USB events
-                const usb_command_executor = usb.CreateAndInitUsbCommandExecutor();
-                while (true) {
-                    // Detect local changes
-                    const current_time = core.TimeSinceBoot{ .time_since_boot_us = time.get_time_since_boot().to_us() };
-                    try matrix_scanner.DetectKeyboardChanges(&matrix_change_queue, current_time); // Scan local matrix changes
-                    try encoder_scanner.detectEncoderChanges(&encoder_change_queue, current_time);
-
-                    // Receive from remote side
-                    if (uart_or_null) |uart| {
-                        try receive_from_uart_to_queue(&uart, &uart_receiver, &matrix_change_queue, current_time);
-                    }
-
-                    // Processing: decide actions
-                    try processor.Process(current_time);
-
-                    // Execute actions: send usb commands to the host
-                    try usb_command_executor.HouseKeepAndProcessCommands(&usb_command_queue, current_time);
-                }
+                try run_primary_internal(self.config, uart);
             }
         };
+
+        fn run_primary_internal(
+            comptime config: Self,
+            uart_or_null: ?rp2xxx.uart.UART,
+        ) !void {
+            // Data queues
+            var matrix_change_queue = core.MatrixStateChangeQueue.Create();
+            var encoder_change_queue = core.EncoderEventQueue.Create();
+            var usb_command_queue = core.OutputCommandQueue.Create();
+
+            // Input scanning
+            const matrix_scanner = comptime matrix_scanning.CreateMatrixScannerType(dimensions, config.pin_cols, config.pin_rows, config.pin_mappings, config.scanner_settings){};
+            var encoder_scanner = comptime encoder_scanning.CreateEncoderScannerType(config.encoder_configs){};
+
+            // Processing
+            var processor = processing.CreateProcessorType(dimensions, config.keymap, config.side_definition, config.combos, config.custom_functions){
+                .input_matrix_changes = &matrix_change_queue,
+                .output_usb_commands = &usb_command_queue,
+                .encoder_event_changes = &encoder_change_queue,
+            };
+
+            // uart byte queue
+            var uart_receiver = split_protocol.UartReceiveHelper{};
+
+            // USB events
+            const usb_command_executor = usb.CreateAndInitUsbCommandExecutor();
+            while (true) {
+                // Detect local changes
+                const current_time = core.TimeSinceBoot{ .time_since_boot_us = time.get_time_since_boot().to_us() };
+                try matrix_scanner.DetectKeyboardChanges(&matrix_change_queue, current_time); // Scan local matrix changes
+                try encoder_scanner.detectEncoderChanges(&encoder_change_queue, current_time);
+
+                // Receive from remote side
+                if (uart_or_null) |uart| {
+                    try receive_from_uart_to_queue(&uart, &uart_receiver, &matrix_change_queue, current_time);
+                }
+
+                // Processing: decide actions
+                try processor.Process(current_time);
+
+                // Execute actions: send usb commands to the host
+                try usb_command_executor.HouseKeepAndProcessCommands(&usb_command_queue, current_time);
+            }
+        }
     };
 }
 
@@ -121,14 +119,13 @@ pub fn GetSecondarySideConfigType(comptime dimensions: *const core.KeymapDimensi
             config: Self,
 
             pub fn run_secondary(comptime self: Runner, uart: rp2xxx.uart.UART) !void {
-                try self.run_secondary_internal(uart);
+                try run_secondary_internal(self.config, uart);
             }
 
             fn run_secondary_internal(
-                comptime self: Runner,
+                comptime config: Self,
                 uart: rp2xxx.uart.UART,
             ) !void {
-                const config = self.config;
                 var matrix_change_queue = core.MatrixStateChangeQueue.Create();
                 const matrix_scanner = comptime matrix_scanning.CreateMatrixScannerType(dimensions, config.pin_cols, config.pin_rows, config.pin_mappings, config.scanner_settings){};
 
@@ -147,7 +144,7 @@ pub fn GetSecondarySideConfigType(comptime dimensions: *const core.KeymapDimensi
     };
 }
 
-pub fn receive_from_uart_to_queue(uart: *const rp2xxx.uart.UART, receiver: *split_protocol.UartReceiveHelper, matrix_change_queue: *core.MatrixStateChangeQueue, current_time: core.TimeSinceBoot) !void {
+fn receive_from_uart_to_queue(uart: *const rp2xxx.uart.UART, receiver: *split_protocol.UartReceiveHelper, matrix_change_queue: *core.MatrixStateChangeQueue, current_time: core.TimeSinceBoot) !void {
     while (uart.read_word() catch {
         uart.clear_errors();
         return;
@@ -163,7 +160,7 @@ pub fn receive_from_uart_to_queue(uart: *const rp2xxx.uart.UART, receiver: *spli
     }
 }
 
-pub fn send_from_queue_to_uart(uart: *const rp2xxx.uart.UART, uart_sender: *split_protocol.UartSendHelper, matrix_change_queue: *core.MatrixStateChangeQueue) !void {
+fn send_from_queue_to_uart(uart: *const rp2xxx.uart.UART, uart_sender: *split_protocol.UartSendHelper, matrix_change_queue: *core.MatrixStateChangeQueue) !void {
     while (matrix_change_queue.dequeue()) |matrix_change| {
         try uart_sender.sendMessage(.{ .MatrixStateChange = .{ .key_index = matrix_change.key_index, .pressed = matrix_change.pressed } });
 
