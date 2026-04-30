471270c45ec921b8f758dc8dd6517503b17b2e2b
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-04-30 09:13:48 +0200

    dasbob example cleanup

--- keyboards/examples/dasbob/keymap.zig
+++ keyboards/examples/dasbob/keymap.zig
@@ -18,26 +18,38 @@ pub const ARROW_KEYS_LAYER = 1;
 pub const keymap = [_][key_count]zigmkay.core.KeyDef{
     // BASE_LAYER 
     .{
-         T(dk.Q), T(dk.W), T(dk.E), T(dk.R), T(dk.T),                  T(dk.Y), T(dk.U), T(dk.I),    T(dk.O),  T(dk.P),
-         T(dk.A), T(dk.S), T(dk.D), T(dk.F), T(dk.G),                  T(dk.H), T(dk.J), T(dk.K),    T(dk.L),  T(dk.SCLN),
-         T(dk.Z), T(dk.X), T(dk.C), T(dk.V), T(dk.B),                  T(dk.N), T(dk.M), T(dk.COMM), T(dk.DOT),T(dk.SLSH),
-                           T(dk.A), T(us.ENTER), T(dk.B),           T(dk.T), T(us.SPACE), T(dk.C)
+         T(dk.Q), T(dk.W), T(dk.E), T(dk.R), T(dk.T),                  T(dk.Y), T(dk.U),   T(dk.I),    T(dk.O),   T(dk.P),
+         T(dk.A), T(dk.S), T(dk.D), T(dk.F), T(dk.G),                  T(dk.H), T(dk.J),   T(dk.K),    T(dk.L),   T(dk.SCLN),
+         T(dk.Z), T(dk.X), T(dk.C), T(dk.V), T(dk.B),                  T(dk.N), T(dk.M),   T(dk.COMM), T(dk.DOT), T(dk.SLSH),
+                           T(dk.A), T(us.ENTER), T(dk.B),              T(dk.T), Space_Num, T(dk.C)
     }, 
     // NUMBER_LAYER
     .{
          ________, ________, ________, ________, ________,                  ________, T(dk.N7), T(dk.N8), T(dk.N9), ________,
          ________, ________, ________, ________, ________,                  ________, T(dk.N4), T(dk.N5), T(dk.N6), ________,
          ________, ________, ________, ________, ________,                  ________, T(dk.N1), T(dk.N2), T(dk.N3), ________,
-                             ________, ________, ________,                  ________,   ________,   ________,
+                             ________, ________, ________,                  ________, ________, ________,
     },
     // ARROW_KEYS_LAYER
     .{
-         ________, ________, ________, ________, ________,                  ________, T(us.HOME),   T(us.UP),   T(us.END), ________,
+         ________, ________, ________, ________, ________,                  ________, T(us.HOME), T(us.UP),   T(us.END),   ________,
          ________, ________, ________, ________, ________,                  ________, T(us.LEFT), T(us.DOWN), T(us.RIGHT), ________,
-         ________, ________, ________, ________, ________,                  ________,   ________,   ________,    ________, ________,
-                             ________, ________, ________,                  ________,   ________,   ________,
+         ________, ________, ________, ________, ________,                  ________, ________,   ________,   ________,    ________,
+                             ________, ________, ________,                  ________, ________,   ________,
     },
 };
+
+pub const Space_Num = core.KeyDef{
+    .tap_hold = .{
+        .tap = .{
+            .key_press = .{ dk.A } 
+        },
+        .hold = .{
+            .hold_layer = NUMBER_LAYER
+        }
+    }
+};
+
 // zig fmt: on
 
 pub const dimensions = core.KeymapDimensions{
