3ee2323b109e7f232132c2cb516fb6eb77216ee8
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-06-17 10:40:56 +0200

    updated leonardo keycaprio v0.2

--- keyboards/my_keyboards/rollercole/leonardo_keycaprio_0_2.zig
+++ keyboards/my_keyboards/rollercole/leonardo_keycaprio_0_2.zig
@@ -14,33 +14,58 @@ const us = zigmkay.keycodes.us;
 pub const pin_config = rp2xxx.pins.GlobalConfiguration{
     .GPIO17 = .{ .name = "led", .direction = .out },
 
-    .GPIO20 = .{ .name = "col0", .direction = .out },
-    .GPIO23 = .{ .name = "col1", .direction = .out },
-    .GPIO21 = .{ .name = "col2", .direction = .out },
-    .GPIO7 = .{ .name = "col3", .direction = .out },
-    .GPIO8 = .{ .name = "col4", .direction = .out },
+    .GPIO2 = .{ .name = "colPinkyL", .direction = .out },
+    .GPIO3 = .{ .name = "colRingL", .direction = .out },
+    .GPIO4 = .{ .name = "colMidL", .direction = .out },
+    .GPIO8 = .{ .name = "colIndexL", .direction = .out },
+    .GPIO9 = .{ .name = "colInnerL", .direction = .out },
+    .GPIO13 = .{ .name = "colThumbL", .direction = .out },
 
-    .GPIO27 = .{ .name = "row0", .direction = .in },
-    .GPIO26= .{ .name = "row1", .direction = .in },
-    .GPIO22 = .{ .name = "row2", .direction = .in },
-    .GPIO4 = .{ .name = "row3", .direction = .in },
-    .GPIO5 = .{ .name = "row4", .direction = .in },
-    .GPIO6 = .{ .name = "row5", .direction = .in },
+    .GPIO7 = .{ .name = "rowTopL", .direction = .in },
+    .GPIO12= .{ .name = "rowHomeL", .direction = .in },
+    .GPIO6 = .{ .name = "rowBottomL", .direction = .in },
+    .GPIO5 = .{ .name = "rowThumbL", .direction = .in },
+
+    .GPIO29 = .{ .name = "colPinkyR", .direction = .out },
+    .GPIO28 = .{ .name = "colRingR", .direction = .out },
+    .GPIO27 = .{ .name = "colMidR", .direction = .out },
+    .GPIO23 = .{ .name = "colIndexR", .direction = .out },
+    .GPIO21 = .{ .name = "colInnerR", .direction = .out },
+    .GPIO26 = .{ .name = "colThumbR", .direction = .out },
+
+    .GPIO20 = .{ .name = "rowTopR", .direction = .in },
+    .GPIO16= .{ .name = "rowHomeR", .direction = .in },
+    .GPIO22 = .{ .name = "rowBottomR", .direction = .in },
+    .GPIO15 = .{ .name = "rowThumbR", .direction = .in },
 };
-pub const p = pin_config.pins();
-
+pub const p = blk: {
+    @setEvalBranchQuota(10_000);
+    break :blk pin_config.pins();
+};
 pub const pin_mappings = [rollercole_shared_keymap.key_count]?[2]usize{
-  .{0,0}, .{1,0}, .{2,0}, .{3,0}, .{4,0},  .{4,3},.{3,3},.{2,3},.{1,3},.{0,3},
-  .{0,1}, .{1,1}, .{2,1}, .{3,1}, .{4,1},    .{4,4},.{3,4},.{2,4},.{1,4},.{0,4},
-          .{1,2}, .{2,2}, .{3,2}, .{4,2},    .{4,5},.{3,5},.{2,5},.{1,5},
-                                 .{0, 2},   .{0, 5}
+  .{0,0}, .{1,0}, .{2,0}, .{3,0}, .{4,0},  .{10,4},.{9,4},.{8,4},.{7,4},.{6,4},
+  .{0,1}, .{1,1}, .{2,1}, .{3,1}, .{4,1},    .{10,5},.{9,5},.{8,5},.{7,5},.{6,5},
+          .{1,2}, .{2,2}, .{3,2}, .{4,2},    .{10,6},.{9,6},.{8,6},.{7,6},
+                                 .{5, 3},   .{11, 7}
 };
 
 // zig fmt: on
-pub const clacky_pin_cols = [_]rp2xxx.gpio.Pin{ p.col0, p.col1, p.col2, p.col3, p.col4 };
-pub const clacky_pin_rows = [_]rp2xxx.gpio.Pin{ p.row0, p.row1, p.row2, p.row3, p.row4, p.row5 };
+pub const pin_cols = [_]rp2xxx.gpio.Pin{
+    //0         1           2           3           4           5
+    p.colPinkyL, p.colRingL, p.colMidL, p.colIndexL, p.colInnerL, p.colThumbL,
+    //6         7           8           9           10          11
+    p.colPinkyR, p.colRingR, p.colMidR, p.colIndexR, p.colInnerR, p.colThumbR,
+};
+
+pub const pin_rows = [_]rp2xxx.gpio.Pin{
+    //0        1           2             3
+    p.rowTopL, p.rowHomeL, p.rowBottomL, p.rowThumbL,
+    //4        5           6             7
+    p.rowTopR, p.rowHomeR, p.rowBottomR, p.rowThumbR,
+};
 
 pub fn main() !void {
+    @setEvalBranchQuota(10_000);
     _ = pin_config.apply();
     blink_led(1, 300); // Show the user that the keyboard has actually booted up.
 
@@ -55,8 +80,8 @@ pub fn main() !void {
                 .matrix = .{
                     .debounce = .{ .ms = 50 },
                     .pins_to_keys_mapping = &pin_mappings,
-                    .pin_cols = clacky_pin_cols[0..],
-                    .pin_rows = clacky_pin_rows[0..],
+                    .pin_cols = pin_cols[0..],
+                    .pin_rows = pin_rows[0..],
                     .direction = .col2row,
                 },
             },
