344c07fc6fd5de316602bb2ecacb857e2fb378da
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-08-08 11:54:32 +0000

    using zig 0.15.2 for the project

--- shell.nix
+++ shell.nix
@@ -2,11 +2,11 @@
 
 pkgs.mkShell {
   packages = with pkgs; [
-    zig
     zls
   ];
 
   shellHook = ''
+    export PATH="${toString ./.}/zig-x86_64-linux-0.15.2:$PATH"
     echo "zig: $(zig version)"
     exec fish
   '';
