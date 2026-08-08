7d79f94e803e3222b9d8a3213bc599790b7d83f9
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-08-08 12:18:19 +0000

    added zls to shell

--- shell.nix
+++ shell.nix
@@ -2,11 +2,11 @@
 
 pkgs.mkShell {
   packages = with pkgs; [
-    zls
   ];
 
   shellHook = ''
     export PATH="${toString ./.}/zig-x86_64-linux-0.15.2:$PATH"
+    export PATH="${toString ./.}/zls-x86_64-linux-0.15.1:$PATH"
     echo "zig: $(zig version)"
     exec fish
   '';
